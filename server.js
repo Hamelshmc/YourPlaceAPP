809ae4a66e0e3f3aa320d76a6e4b7bc1a01de420
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2021-02-14 21:53:23 +0100

    [ADD] Server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const app = require('./src');
+
 const PORT = process.env.PORT || 8080;
 // starting the server
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}!`));
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const compression = require('compression');
 const cors = require('cors');
 const errorMiddleware = require('./middleware/error.middleware');
 const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const corsOptions = require('./middleware/cors.middleware');
 
 const bookingsRouter = require('./api/booking/routes/booking.routes');
 const contractsRouter = require('./api/contract/routes/contract.routes');
@@ -19,25 +22,7 @@ const app = express();
 app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-// # Do the same for the following code. Your main server file that is.
-//  MIDDLEWARE
-const whitelist = [
-  'http://localhost:3000',
-  'http://localhost:8080',
-  'https://yourplaceapp.herokuapp.com',
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log('** Origin of request ' + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log('Origin acceptable');
-      callback(null, true);
-    } else {
-      console.log('Origin rejected');
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+app.use(helmet());
 app.use(cors(corsOptions));
 
 app.use('/api/v1/bookings', bookingsRouter);
@@ -49,22 +34,16 @@ app.use('/api/v1/transactions', transactionsRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/visits', visitsRouter);
 
-//  Serve client react instead of backend
-//  Add the follwing code to your server file on the backend
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 // Endpoint Not Found
-app.all('/api/v1/', (req, res, next) => {
+app.all('/api/*', (req, res, next) => {
   const err = new ResponseError(httpStatus.NOT_FOUND, 'Endpoint Not Found');
   next(err);
+});
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // Error middleware
