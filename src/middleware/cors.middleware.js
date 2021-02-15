563353d9936cf47b5738655045ae1370476cdb0d
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2021-02-15 11:42:03 +0100

    [ADD] CorsOptions WhiteList

---
+++ src/middleware/cors.middleware.js
@@ -0,0 +1,20 @@
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:8080',
+  'https://yourplaceapp.herokuapp.com',
+  'https://yourplaceappdev.herokuapp.com',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log('** Origin of request ' + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log('Origin acceptable');
+      callback(null, true);
+    } else {
+      console.log('Origin rejected');
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+module.exports = corsOptions;
