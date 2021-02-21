c0c85b9dc646c751f47149a15fb0b8c2c78a1def
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2021-02-21 15:16:16 +0100

    [FIX]

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import GlobalStyle from './theme/GlobalStyle';
 import Theme from './theme/Theme';
 
 const Home = React.lazy(() => import('./pages/home'));
@@ -8,17 +7,14 @@ const SingIn = React.lazy(() => import('./pages/singIn'));
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Theme>
-        <Suspense fallback>
-          <Router>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/sign_in" component={SingIn} />
-          </Router>
-        </Suspense>
-      </Theme>
-    </>
+    <Suspense fallback>
+      <Router>
+        <Theme>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/sign_in" component={SingIn} />
+        </Theme>
+      </Router>
+    </Suspense>
   );
 }
 
