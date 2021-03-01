037ef8e12a981d51d9eea7f8da7a9c68db648e39
msieiro <sieirogutierrezmartin@gmail.com>
2021-03-01 14:17:46 +0100

    [ADD] visit component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Visit from '../components/Visit/Visit';
 import WelcomeContainer from '../components/Welcome/Welcome';
 const Home = () => (
   <div>
     <WelcomeContainer />
+    <Visit />
   </div>
 );
 
