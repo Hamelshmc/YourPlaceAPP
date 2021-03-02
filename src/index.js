adc623ce6129a1580a76361f15c50f95ec4a18a8
msieiro <sieirogutierrezmartin@gmail.com>
2021-03-02 19:05:26 +0100

    [FIX] CORS WHITELIST

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(configureLog());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
