f89ceb0f12314f45a2309faeddbb3345272e9882
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2021-03-10 17:05:42 +0100

    [FIX] Format

--- client/src/components/Profile/Header.js
+++ client/src/components/Profile/Header.js
@@ -7,35 +7,33 @@ import UserAvatar from './styles/UserAvatar';
 import Username from './styles/Username';
 import UserProfile from './styles/UserProfile';
 
-const Header = ({ user }) => {
-  return (
-    <ProfileHeader>
-      <ProfileHeaderContainer>
-        <ProfileHeaderContent>
-          <UserProfile>
-            <UserAvatar
-              src={user && user.picture ? user.picture : '/assets/User.svg'}
-              loading="lazy"
-              alt="profile image"
-              width=""
-              height="100"
+const Header = ({ user }) => (
+  <ProfileHeader>
+    <ProfileHeaderContainer>
+      <ProfileHeaderContent>
+        <UserProfile>
+          <UserAvatar
+            src={user && user.picture ? user.picture : '/assets/User.svg'}
+            loading="lazy"
+            alt="profile image"
+            width=""
+            height="100"
             />
-            <Username>
-              <span>
-                {user && user.email
+          <Username>
+            <span>
+              {user && user.email
                   ? user.email.split('@')[0]
                   : 'Jhon Doe'
                   ? user && user.fullName
                   : user.fullName}
-              </span>
-            </Username>
-          </UserProfile>
-        </ProfileHeaderContent>
-        <EditProfile>
-          <EditButton>Edit profile</EditButton>
-        </EditProfile>
-      </ProfileHeaderContainer>
-    </ProfileHeader>
+            </span>
+          </Username>
+        </UserProfile>
+      </ProfileHeaderContent>
+      <EditProfile>
+        <EditButton>Edit profile</EditButton>
+      </EditProfile>
+    </ProfileHeaderContainer>
+  </ProfileHeader>
   );
-};
 export default Header;
