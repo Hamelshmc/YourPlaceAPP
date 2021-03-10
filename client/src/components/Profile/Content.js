6a6714708c87246e6d1ce87eff9508222b368c5a
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2021-03-10 10:22:03 +0100

    [FIX]

--- client/src/components/Profile/Content.js
+++ client/src/components/Profile/Content.js
@@ -7,22 +7,18 @@ import UserRating from './styles/UserRating';
 
 const Content = ({ user }) => (
   <BioInfoContainer>
-    <BioInfoItem>{user.bio ? user.bio : ''}</BioInfoItem>
+    <BioInfoItem>{user ? user.bio : ''}</BioInfoItem>
     <BioInfoItemEmphasized>
       <Icon>location_on</Icon>
-      {user.city ? `${user.city}, ` : ''} {user.country ? user.country : 'España'}
+      {user ? `${user.city}, ` : ''} {user ? user.country : 'España'}
     </BioInfoItemEmphasized>
     <BioInfoItemEmphasized>
       <Icon>calendar_today</Icon>
-      {user.borndate ? user.borndate : ''}
+      {user ? user.borndate : ''}
     </BioInfoItemEmphasized>
 
     <UserRating>
-      {user.userRating ? (
-        <StartRating value={user.userRating} disabled />
-      ) : (
-        <StartRating value={0} disabled />
-      )}
+      {user ? <StartRating value={user.userRating} disabled /> : <StartRating value={0} disabled />}
     </UserRating>
   </BioInfoContainer>
 );
