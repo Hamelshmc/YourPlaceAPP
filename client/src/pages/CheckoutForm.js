89f6ebb88823cf1ed7b72b5ff19458afd472956a
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2021-03-16 11:42:58 +0100

    [ADD] Style Payment

--- client/src/pages/CheckoutForm.js
+++ client/src/pages/CheckoutForm.js
@@ -3,8 +3,33 @@ import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import styled, { keyframes } from 'styled-components';
 import fetchCreateTransaction from '../api/Transaction';
 import { UserContext } from '../hooks/UserContext';
+
+const cardStyle = {
+  iconStyle: 'solid',
+  style: {
+    base: {
+      iconColor: '#c4f0ff',
+      color: '#fff',
+      fontWeight: 500,
+      fontFamily: 'Nunito, Open Sans, Segoe UI, sans-serif',
+      fontSize: '16px',
+      fontSmoothing: 'antialiased',
+      ':-webkit-autofill': {
+        color: '#fce883',
+      },
+      '::placeholder': {
+        color: '#87bbfd',
+      },
+    },
+    invalid: {
+      iconColor: '#ffc7c7',
+      color: '#ffc7ee',
+    },
+  },
+};
 
 const CheckoutForm = () => {
   const [user, setUser] = useContext(UserContext);
@@ -13,6 +38,9 @@ const CheckoutForm = () => {
   const [processing, setProcessing] = useState('');
   const [disabled, setDisabled] = useState(true);
   const [clientSecret, setClientSecret] = useState('');
+  const [cardComplete, setCardComplete] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState(null);
+
   const stripe = useStripe();
   const elements = useElements();
   const { id } = useParams();
@@ -32,24 +60,6 @@ const CheckoutForm = () => {
       });
   }, []);
 
-  const cardStyle = {
-    style: {
-      base: {
-        color: '#32325d',
-        fontFamily: 'Arial, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-          color: '#32325d',
-        },
-      },
-      invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a',
-      },
-    },
-  };
-
   const handleChange = async (event) => {
     // Listen for changes in the CardElement
     // and display any errors as the customer types their card details
@@ -59,7 +69,20 @@ const CheckoutForm = () => {
 
   const handleSubmit = async (ev) => {
     ev.preventDefault();
-    setProcessing(true);
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    if (error) {
+      elements.getElement('card').focus();
+      return;
+    }
+    if (cardComplete) {
+      setProcessing(true);
+    }
 
     const payload = await stripe
       .confirmCardPayment(clientSecret, {
@@ -76,6 +99,12 @@ const CheckoutForm = () => {
         }
       });
 
+    if (payload.error) {
+      setError(payload.error);
+    } else {
+      setPaymentMethod(payload.paymentMethod);
+    }
+
     /* if (payload.error) {
       setError(`Payment failed ${payload.error.message}`);
       setProcessing(false);
@@ -87,15 +116,150 @@ const CheckoutForm = () => {
   };
 
   return (
-    <form id="payment-form" onSubmit={handleSubmit}>
-      <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
-      <button disabled={processing || disabled || succeeded} id="submit">
-        <span id="button-text">
-          {processing ? <div className="spinner" id="spinner" /> : 'Pay now'}
-        </span>
-      </button>
-    </form>
+    <SectionStyle>
+      <FormStyle onSubmit={handleSubmit}>
+        <FormGroup>
+          <CardField
+            onChange={(e) => {
+              setError(e.error);
+              setCardComplete(e.complete);
+            }}
+          />
+        </FormGroup>
+        {error && <ErrorMessage>{error.message}</ErrorMessage>}
+        <SubmitButton processing={processing} error={error} disabled={!stripe}>
+          Pay
+        </SubmitButton>
+      </FormStyle>
+    </SectionStyle>
   );
 };
 
 export default CheckoutForm;
+
+const CardField = ({ onChange }) => (
+  <FormRow>
+    <CardElementStyle options={cardStyle} onChange={onChange} />
+  </FormRow>
+);
+
+const SubmitButton = ({ processing, error, children, disabled }) => (
+  <SubmitButtonStyle error type="submit" disabled={processing || disabled}>
+    {processing ? 'Processing...' : children}
+  </SubmitButtonStyle>
+);
+
+const ErrorMessage = ({ children }) => (
+  <ErrorMessageStyle role="alert">
+    <svg width="16" height="16" viewBox="0 0 17 17">
+      <path
+        fill="#FFF"
+        d="M8.5,17 C3.80557963,17 0,13.1944204 0,8.5 C0,3.80557963 3.80557963,0 8.5,0 C13.1944204,0 17,3.80557963 17,8.5 C17,13.1944204 13.1944204,17 8.5,17 Z"
+      />
+      <path
+        fill="#6772e5"
+        d="M8.5,7.29791847 L6.12604076,4.92395924 C5.79409512,4.59201359 5.25590488,4.59201359 4.92395924,4.92395924 C4.59201359,5.25590488 4.59201359,5.79409512 4.92395924,6.12604076 L7.29791847,8.5 L4.92395924,10.8739592 C4.59201359,11.2059049 4.59201359,11.7440951 4.92395924,12.0760408 C5.25590488,12.4079864 5.79409512,12.4079864 6.12604076,12.0760408 L8.5,9.70208153 L10.8739592,12.0760408 C11.2059049,12.4079864 11.7440951,12.4079864 12.0760408,12.0760408 C12.4079864,11.7440951 12.4079864,11.2059049 12.0760408,10.8739592 L9.70208153,8.5 L12.0760408,6.12604076 C12.4079864,5.79409512 12.4079864,5.25590488 12.0760408,4.92395924 C11.7440951,4.59201359 11.2059049,4.59201359 10.8739592,4.92395924 L8.5,7.29791847 L8.5,7.29791847 Z"
+      />
+    </svg>
+    {children}
+  </ErrorMessageStyle>
+);
+
+const FormRow = styled.section`
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-left: 15px;
+  border-top: 1px solid #819efc;
+  &:first-child {
+    border-top: none;
+  }
+`;
+
+const ErrorMessageStyle = styled.div`
+  color: #fff;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  padding: 0 15px;
+  font-size: 13px;
+  margin-top: 0px;
+  width: 100%;
+  transform: translateY(-15px);
+  opacity: 0;
+  animation: fade 150ms ease-out;
+  animation-delay: 50ms;
+  animation-fill-mode: forwards;
+  will-change: opacity, transform;
+  & svg {
+    margin-right: 10px;
+  }
+`;
+
+const SubmitButtonStyle = styled.button`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline: none;
+  border-style: none;
+  display: block;
+  font-size: 16px;
+  width: calc(100% - 30px);
+  height: 40px;
+  margin: 40px 15px 0;
+  background-color: #7d1735;
+  box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08), inset 0 1px 0 #ffb9f6;
+  border-radius: 4px;
+  color: #fff;
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 100ms ease-in-out;
+  will-change: transform, background-color, box-shadow;
+  &:active {
+    background-color: #d782d9;
+    box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08),
+      inset 0 1px 0 #e298d8;
+    ${({ error }) => (error ? 'transform: translateY(15px)' : 'transform: scale(0.99)')}
+  }
+  ${({ error }) => error && 'transform: translateY(15px)'}
+`;
+
+const fade = keyframes`
+  from {
+    opacity: 0;
+    transform: scale3D(0.95, 0.95, 0.95);
+  }
+  to {
+    opacity: 1;
+    transform: scale3D(1, 1, 1);
+  }
+`;
+
+const FormStyle = styled.form`
+  animation: ${fade} 200ms ease-out;
+`;
+
+const FormGroup = styled.fieldset`
+  margin: 0 15px 20px;
+  padding: 0;
+  border-style: none;
+  background-color: #0f58aa;
+  will-change: opacity, transform;
+  box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08), inset 0 1px 0 #829fff;
+  border-radius: 4px;
+`;
+const SectionStyle = styled.section`
+  height: 100%;
+  margin: auto;
+  width: clamp(20rem, 50%, 28rem);
+  min-height: 4rem;
+  max-height: 12rem;
+  padding: 0.5rem;
+  position: relative;
+`;
+
+const CardElementStyle = styled(CardElement)`
+  width: 100%;
+  padding: 1rem 1rem 1rem 0;
+`;
