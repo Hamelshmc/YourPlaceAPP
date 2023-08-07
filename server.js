db1468405f991dca7679f503342d4f4c29c6882b
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2023-08-07 14:14:44 +0200

    💥 BREAKING CHANGE:  :(

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const https = require('https');
-const fs = require('fs');
 const app = require('./src');
 
 process.on('unhandledRejection', (reason, promise) => {
@@ -8,11 +6,5 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 const PORT = process.env.PORT || 8080;
-
-const privateKey = fs.readFileSync('./key.pem', 'utf8');
-const certificate = fs.readFileSync('./cert.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const server = https.createServer(credentials, app);
-
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}!`));
+// starting the server
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}!`));
